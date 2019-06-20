b2908929da7060d607d77ae46d45e04f1d1a2010
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-06-20 22:55:44 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from time import sleep, 
+from time import sleep
 from random import randint
 import pickle 
 #------------------------------------------------------------------------------------------------------------------------------
